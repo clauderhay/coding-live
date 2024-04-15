a4cbd6e2f156d486006b5df8f594589155e96d43
Claude Rhay <claude.rhay@gmail.com>
2024-04-15 21:57:17 +0800

    Live Coding - 1: Opening a File

--- Program.cs
+++ Program.cs
@@ -1,4 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
 
-test
+class Program
+{
+    static void Main(string[] args)
+    {
+        //OPENFILE FUNC
+        Console.WriteLine("Enter file path: ");
+        string filePath = Console.ReadLine();
+
+        try
+        {
+            OpenFile(filePath);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Something went wrong: {ex.Message}");
+        }
+
+        Console.ReadLine();
+     
+    }
+
+    // Open File from a user
+    static void OpenFile( string filePath)
+    {
+        // Opening the file
+        using (FileStream fs = File.Open(filePath, FileMode.Open))
+        {
+            Console.Write("File opened successfully.");
+        }
+    }
+
+}
