dd864ebb6cdbb2f7b491415b26c9884b4e70d890
Claude Rhay <claude.rhay@gmail.com>
2024-04-15 22:04:39 +0800

    Live-Coding 2: Valid Input

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         //OPENFILE FUNC
-        Console.WriteLine("Enter file path: ");
+        /*Console.WriteLine("Enter file path: ");
         string filePath = Console.ReadLine();
 
         try
@@ -21,13 +21,29 @@ class Program
         {
             Console.WriteLine($"Something went wrong: {ex.Message}");
         }
+        */
+
+        //VALIDNUM FUNC
+        try
+        {
+            int number = GetValidNumber();
+            Console.WriteLine("The number entered is: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalud input");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Something went wrong: {ex.Message}");
+        }
 
         Console.ReadLine();
-     
+
     }
 
     // Open File from a user
-    static void OpenFile( string filePath)
+    static void OpenFile(string filePath)
     {
         // Opening the file
         using (FileStream fs = File.Open(filePath, FileMode.Open))
@@ -35,5 +51,21 @@ class Program
             Console.Write("File opened successfully.");
         }
     }
+
+    // Integer 0-1000
+    static int GetValidNumber()
+    {
+        Console.Write("Enter a valid number: ");
+        string input = Console.ReadLine();
+        int num = Convert.ToInt32(input);
+
+        if (num < 0 || num > 1000)
+        {
+            throw new ArgumentOutOfRangeException("Must be between 0 and 1000");
+        }
+
+        return num;
+    }
+
 
 }
